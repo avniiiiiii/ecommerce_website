6060f834411dfe7479cf17eb4d1c6559d3365aff
avni <avniiiiiii07@gmail.com>
2024-04-14 22:04:35 +0530

    push

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = process.env.PORT; // Use PORT from environment variable or default to 3000
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const port = process.env.PORT; // Assign a default port if PORT environment variable is not set
+const Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-console.log("hello");
 
-app.listen(port, () => {
-  console.log(`Server is running on Port ${port}`);
-});
 app.post("/pay", async (req, res) => {
   console.log(req.body.token);
   await Stripe.charges.create({
